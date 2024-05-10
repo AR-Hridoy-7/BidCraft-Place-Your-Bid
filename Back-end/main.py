19d6076dd94fd53df1ee27fe6b307168906b821a
AR-Hridoy-7 <arhridoy2002@gmail.com>
2024-05-10 20:15:11 +0600

    Updated

--- Back-end/main.py
+++ Back-end/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 import models
+import utils
 from database import engine, get_db
 from routers import user,item,category,auth,qa,user_rating,bid
-
-app = FastAPI()
+import config
 
 models.Base.metadata.create_all(engine)
+app = FastAPI()
+
+manager = utils.connectionManager()
+
+# Define allowed origins
+origins = [
+    "http://localhost",
+    "http://localhost:3000",  # Add your frontend URL
+]
+
+# Configure CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 app.include_router(user.router)
 app.include_router(item.router)
@@ -14,7 +33,3 @@ app.include_router(auth.router)
 app.include_router(qa.router)
 app.include_router(user_rating.router)
 app.include_router(bid.router)
-
-app.get("/api")
-async def root():
-  return {"message": "Awesome app"}
